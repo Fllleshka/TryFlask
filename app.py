19f684ad5d5545a30085a7bbc2b622cfe5c92bb5
Fleshka <fleisner.omsk@gmail.com>
2024-02-07 16:53:03 +0600

    Добавлена возможность добавления поста

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 from helpfunctions import *
 
@@ -8,6 +8,7 @@ app = Flask(__name__)
 def before_first_reqest():
     init_db()
 
+# Путь до главного(корневого) файла
 @app.route('/')
 def index():
     conn = get_db_connection()
@@ -15,13 +16,29 @@ def index():
     conn.close()
     return render_template('index.html', posts=posts)
 
+# Путь до поста
 @app.route('/<int:post_id>')
 def get_post(post_id):
     conn = get_db_connection()
-    conn.execute('INSERT INTO posts (title, content) VALUES ("Random Title", "Lorem ipsum dolor sit amet consectetur adipiscing elit")')
     post = conn.execute('SELECT * FROM posts WHERE id = ?', (post_id,)).fetchone()
     conn.close()
     return render_template('post.html', post=post)
+
+# Путь для новго поста
+@app.route('/new', methods=['GET', 'POST'])
+def new_post():
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+
+        conn = get_db_connection()
+        conn.execute('INSERT INTO posts (title, content) VALUES (?, ?)', (title, content))
+        conn.commit()
+        conn.close()
+
+        return redirect(url_for('index'))
+
+    return render_template('add_post.html')
 
 if __name__ == '__main__':
     app.run()
